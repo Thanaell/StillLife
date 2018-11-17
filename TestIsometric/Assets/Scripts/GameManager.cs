c7054035d20f586d243ad109ce5dbbd61f7dccf0
Thanaell <gaelle_clavelin@orange.fr>
2018-11-17 20:43:53 +0100

    main menu

--- TestIsometric/Assets/Scripts/GameManager.cs
+++ TestIsometric/Assets/Scripts/GameManager.cs
@@ -5,10 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class GameManager: Singleton<GameManager> {
 
+    public GameObject creditsImage;
     protected GameManager() { }
 
+    private void Update()
+    {
+      if (creditsImage.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                creditsImage.SetActive(false);
+            }
+        }
+    }
     public void LoadGame()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadCredits()
+    {
+        creditsImage.SetActive(true);
     }
 }
