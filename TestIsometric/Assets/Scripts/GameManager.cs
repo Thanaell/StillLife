4ff465ba10482ade5d17cc5c9faa734d63964588
Randrianantoanina <rova.randrian@gmail.com>
2018-11-17 12:17:20 +0100

    Correction collider et nettoyage

---
+++ TestIsometric/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager: Singleton<GameManager> {
+
+    protected GameManager() { }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
