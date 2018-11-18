f0b37bfca7b9037f3b03102a9121f580318f0c1b
Thanaell <gaelle_clavelin@orange.fr>
2018-11-18 08:24:39 +0100

    script d'interaction avec les pilules (à ajuster comme on veut pour le feedback)

---
+++ TestIsometric/Assets/Scripts/InteractionPills.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionPills : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("bonne pilule");
+        }
+        else if (Input.anyKeyDown)
+        {
+            Debug.Log("t'es nul ou quoi ?");
+        }
+    }
+}
