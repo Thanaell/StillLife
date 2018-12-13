218c416cffe1803e8ab43d88b787672500668805
Thanaell <gaelle_clavelin@orange.fr>
2018-12-13 10:25:35 +0100

    arrêt de l'horloge au bout de 10 secondes le dernier jour

---
+++ StillLife/Assets/Scripts/StopOnLastDay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopOnLastDay : MonoBehaviour {
+
+    float timer;
+    public float timeBeforeMute;
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (DayManager.Instance.currentDay.dayNumber == 6)
+        {
+            timer += Time.deltaTime;
+           // Debug.Log(timer);
+            if(timer > timeBeforeMute)
+            {
+                GetComponent<AudioSource>().mute = true;
+            }
+        }
+	}
+}
